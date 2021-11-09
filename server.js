2fbc71d5c9c9404fd6999577c3d4622bad57bd27
Bouyssel Camille <camille.bouyssel@student-cs.fr>
2021-11-09 11:29:10 +0100

    connect to mongodb

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('./config');
 const app = express();
-app.get('/', (req, res) => {
-   res.send('Hello World');
-});
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => console.log(err));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
