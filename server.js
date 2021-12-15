8817e8490194c0a735b3fa12bf6f02056506fd16
jeanneleparc <jeanne.leparc@wanadoo.fr>
2021-12-15 15:53:03 +0100

    Ajout consommateur information et status en cours

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const mongoose = require('mongoose');
 const app = express();
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/citibikes?retryWrites=true&w=majority';
+// const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://user_camille_clara_jeanne:wCXS8PPZcSMdQGTE@cluster0.lenqb.mongodb.net/citibikes_nyc?retryWrites=true&w=majority';
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => console.log('MongoDB connected!'))
+  .then(() => console.log('MongoDB connected : %s', MONGO_URI))
   .catch(err => console.log(err));
 
 const processor = require('./src/processor');
+// const processor = require('./src/processor-status');
 
 processor.main();
 
