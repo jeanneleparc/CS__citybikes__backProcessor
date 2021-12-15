7e7b510c4cc10a802264ce81bb52c7e05c18922c
jeanneleparc <jeanne.leparc@wanadoo.fr>
2021-12-15 16:43:33 +0100

    Ajour des status en bdd

--- src/processor-status.js
+++ src/processor-status.js
@@ -22,30 +22,27 @@ async function main() {
 
             console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
-            channel.consume(queue, function(msg) {
+            channel.consume(queue, async function(msg) {
                 console.log(" [x] Received");
                 const stations = JSON.parse(msg.content).data.stations;
                 const time = JSON.parse(msg.content).last_updated;
-                // const status = await StationStatus.aggregate([{
-                //     $group : { _id: null, max: { $max : "$last_updated" }}
-                // }]).exec();
-
-                const agg = StationInformation.aggregate([{ $group: { _id: null, max: { $max : "$last_updated" }}}]);
-                for await (const doc of agg) {
-                    console.log(doc);
-                }
-                // stations.forEach((station) => {
-
-                //     var status = new StationStatus();
-
-                //     information.id = parseInt(station.station_id, 10);
-                //     information.name = station.name;
-                //     information.longitude = station.lon;
-                //     information.latitude = station.lat;
-                //     information.capacity = station.capacity;
-                //     information.last_updated = moment(time*1000).subtract(5, 'hours');
-                //     information.save(function (err) {});
-                // });
+                const object_last_updated = await StationInformation.find({},{last_updated:1}).sort({'last_updated':-1}).limit(1);
+                const informations = await StationInformation.find({last_updated: object_last_updated[0].last_updated},);
+                stations.forEach((station) => {
+                    var status = new StationStatus();
+                    status.id = parseInt(station.station_id, 10);
+                    status.num_docks_available = station.num_docks_available;
+                    status.num_bikes_available = station.num_bikes_available;
+                    status.station_status = station.station_status;
+                    status.is_installed = station.is_installed;
+                    status.last_updated = moment(time*1000).subtract(5, 'hours');
+                    const info_station = informations.filter((data) => { return data.id == status.id});
+                    status.name = info_station[0].name;
+                    status.longitude = info_station[0].longitude;
+                    status.latitude = info_station[0].latitude;
+                    status.capacity = info_station[0].capacity;
+                    status.save(function (err) {});
+                });
                 console.log("Save In DB");
             }, {
                 noAck: true
