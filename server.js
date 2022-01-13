ba4d59b72b3748450119d84677dd0165dfba9a88
BOUYSSEL Camille <45000497+BouysselCamille@users.noreply.github.com>
2022-01-13 10:57:21 +0100

    Add .circleci/config.yml (#6)
    * Add .circleci/config.yml
    
    * add prettier and eslint
    
    * config circle ci

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/citibikes';
+const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/citibikes";
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('MongoDB connected : %s', MONGO_URI))
-  .catch(err => console.log(err));
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected : %s", MONGO_URI))
+  .catch((err) => console.log(err));
 
-const processor = require('./src/processor');
+const processor = require("./src/processor");
 
 processor.main();
 
