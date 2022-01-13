7fb77da9f31f11219f4f79942dd7bd1090a36b2b
jeanneleparc <jeanne.leparc@wanadoo.fr>
2022-01-13 16:59:26 +0100

    Prettier bis

--- script_mock_data.js
+++ script_mock_data.js
@@ -22,8 +22,8 @@ async function main() {
   const stationInformation = await get(url);
   const todayDate = moment().utc().subtract(5, "hours").startOf("day");
   const nbStations = 50;
-	const nbDays = 30;
-	const nbTimeSlots = 23;
+  const nbDays = 30;
+  const nbTimeSlots = 23;
 
   try {
     await mongoose
@@ -34,34 +34,40 @@ async function main() {
       .then(() => console.log("MongoDB connected : %s", MONGO_URI))
       .catch((err) => console.log(err));
 
-    stationInformation.data.stations.slice(0, nbStations).forEach((station, index) => {
-      const { capacity } = station;
-      for (let i = nbDays; i >= 0; i -= 1) {
-        const currentMockDate = todayDate.clone().subtract(i, "days");
-        for (let j = 0; j <= nbTimeSlots; j += 1) {
-          const stat = new StatsByStationByHour();
-          stat.station_id = parseInt(station.station_id, 10);
-          stat.station_name = station.name;
-          stat.station_long = station.lon;
-          stat.station_lat = station.lat;
-          stat.time_slot = j;
-          const rate = Math.random();
-          stat.filling_rate = rate;
-          stat.avg_bikes_nb = rate * capacity;
-          stat.date = currentMockDate;
-          stat.save((err) => {
-            if (err) return console.error(err);
-            if (((index+1) % 10 === 0 || (index+1)===nbStations) && i === nbDays && j === 0) {
-              console.log(index+1, "/", nbStations);
-            }
-            if ((index+1) === nbStations && i === 0 && j === nbTimeSlots) {
-              process.exit(0);
-            }
-            return null;
-          });
+    stationInformation.data.stations
+      .slice(0, nbStations)
+      .forEach((station, index) => {
+        const { capacity } = station;
+        for (let i = nbDays; i >= 0; i -= 1) {
+          const currentMockDate = todayDate.clone().subtract(i, "days");
+          for (let j = 0; j <= nbTimeSlots; j += 1) {
+            const stat = new StatsByStationByHour();
+            stat.station_id = parseInt(station.station_id, 10);
+            stat.station_name = station.name;
+            stat.station_long = station.lon;
+            stat.station_lat = station.lat;
+            stat.time_slot = j;
+            const rate = Math.random();
+            stat.filling_rate = rate;
+            stat.avg_bikes_nb = rate * capacity;
+            stat.date = currentMockDate;
+            stat.save((err) => {
+              if (err) return console.error(err);
+              if (
+                ((index + 1) % 10 === 0 || index + 1 === nbStations) &&
+                i === nbDays &&
+                j === 0
+              ) {
+                console.log(index + 1, "/", nbStations);
+              }
+              if (index + 1 === nbStations && i === 0 && j === nbTimeSlots) {
+                process.exit(0);
+              }
+              return null;
+            });
+          }
         }
-      }
-    });
+      });
   } catch (e) {
     console.error(e);
   }
