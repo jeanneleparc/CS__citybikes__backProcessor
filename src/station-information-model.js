96b5511621889bce7c472f8f99295d761d648752
Bouyssel Camille <camille.bouyssel@student-cs.fr>
2022-01-13 10:39:48 +0100

    add prettier and eslint

--- src/station-information-model.js
+++ src/station-information-model.js
@@ -1,17 +1,21 @@
-var mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-var stationInformationSchema = mongoose.Schema({
-    id: Number,
-    capacity: Number,
-    has_kiosk: Boolean,
-    last_updated: Date,
-    latitude: Number,
-    longitude: Number,
-    name: String,
+const stationInformationSchema = mongoose.Schema({
+  id: Number,
+  capacity: Number,
+  has_kiosk: Boolean,
+  last_updated: Date,
+  latitude: Number,
+  longitude: Number,
+  name: String,
 });
 
-var stationInformation = module.exports = mongoose.model('stationinformation', stationInformationSchema);
+const stationInformation = mongoose.model(
+  "stationinformation",
+  stationInformationSchema
+);
+module.exports = stationInformation;
 
 module.exports.get = function (callback, limit) {
-    stationInformation.find(callback).limit(limit);
-}
+  stationInformation.find(callback).limit(limit);
+};
