c142eb80d5ca0c1cba7fd14373b79d9f77b3e080
jeanneleparc <jeanne.leparc@wanadoo.fr>
2022-01-19 17:02:25 +0100

    [WIP] cron task analytics

--- src/processor.js
+++ src/processor.js
@@ -1,12 +1,71 @@
 // Import contact model
 const moment = require("moment");
 const amqp = require("amqplib/callback_api");
+const cron = require('node-cron');
+
 const StationStatus = require("./station-status-model");
 const StationInformation = require("./station-information-model");
+const StatsByStationByHour = require("./stats-by-station-by-hour-model");
 
 const AMQP_URL = process.env.AMQP_URL || "amqp://localhost";
 
 async function main() {
+  //Cron task to compute statistics
+cron.schedule('*/10 * * * *', async function() {
+    const todayDateEnd = moment().utc().subtract(5, "hours").startOf("hours");
+    const todayDateBegin = todayDateEnd.clone().subtract(1, "hour");
+    const timeSlot = parseInt(todayDateBegin.clone().format("HH"),10);
+    console.log(todayDateBegin);
+    console.log(timeSlot);
+    const todayDateGlobal = todayDateBegin.clone().startOf("day");
+    let status;
+    try {
+      status = await StationStatus.find({
+        last_updated: {$gte: todayDateBegin,$lt:todayDateEnd},
+      });
+    } finally {
+      const listWithoutCapacity = status.reduce(function(prev, curr) {
+        if(curr.capacity === 0 && !prev.includes(curr.id)){
+          prev.push(curr.id);
+        }
+        return prev;
+      }, []);
+      console.log(status.slice(0, 1));
+      console.log(listWithoutCapacity);
+      const listStationIds = status.reduce(function(prev, curr) {
+        if(!listWithoutCapacity.includes(curr.id) && !prev.includes(curr.id)){
+          prev.push(curr.id);
+        }
+        return prev;
+      }, []);
+      console.log(listStationIds);
+      listStationIds.forEach((stationId, index) => {
+        const statusForStationId = status.filter(x => x.id === stationId);
+        const stat = new StatsByStationByHour();
+        stat.station_id = stationId;
+        stat.station_name = statusForStationId[0].name;
+        stat.station_long = statusForStationId[0].longitude;
+        stat.station_lat = statusForStationId[0].latitude;
+        stat.time_slot = timeSlot;
+        const sum = statusForStationId.reduce(function (accumulator, currentValue){
+          return accumulator + currentValue.num_bikes_available;
+        }, 0);
+        const avg = sum / statusForStationId.length;
+        
+        stat.filling_rate = avg / statusForStationId[0].capacity;
+        stat.avg_bikes_nb = avg;
+        stat.date = todayDateGlobal;
+        stat.save((err) => {
+          if (err) return console.error(err);
+          if (index === listStationIds.length - 1) {
+            console.log('Analytics Computed and Saved');
+          }
+          return null;
+        });
+      });
+    }
+  });
+
   amqp.connect(AMQP_URL, (error0, connection) => {
     if (error0) {
       throw error0;
